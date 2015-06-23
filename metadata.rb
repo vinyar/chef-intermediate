a44bf528b9c305b822698762b6aced245b2fe750
Franklin Webber <franklin.webber@gmail.com>
2015-06-23 00:29:03 -0700

    This is a rudimentary release mechanism
    Use rake:release to create an archive (mac) and then copy into Google Drive (only if syncronized)

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name              'chef-intermediate'
+maintainer        'Chef Software, Inc.'
+maintainer_email  'training@chef.io'
+license           'Creative Commons Attribution-ShareALike 4.0 International (CC BY-SA 4.0)'
+description       'Chef Intermediate Content'
+version           '1.1.4'
+
+asset             "Chef_Intermediate.key", "Presentation (Keynote)"
+asset             "Chef_Intermediate.pdf", "Presentation (PDF)"
+asset             "Chef_Intermediate_Topics-StudentHandouts.docx", "Attendee Handouts (Word)"
+asset             "Chef_Intermediate_Topics-StudentHandouts.pdf", "Attendee Handouts (PDF)"
+
+local_release_folder "~/Google\\ Drive/Training/Chef\\ Intermediate\\ Training/Releases"
